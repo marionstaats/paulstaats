fc506b0fdfc153547a3df69bf3c7cbd748e59316
Marion Staats <marion.staats@intix.eu>
2023-05-15 20:28:39 +0200

    Revert "Put package-lock.json in gitignore"
    This reverts commit db5f3a14676b19c0c642393db777392108e7cbc1.

--- src/vuetifyConfig.ts
+++ src/vuetifyConfig.ts
@@ -9,8 +9,6 @@ export default {
           primary: colors.red.darken1,
           secondary: colors.red.lighten4,
 
-          logo: '#ffcc04',
-
           background: colors.grey.lighten5,
           backgroundNav: colors.grey.lighten4
         }
