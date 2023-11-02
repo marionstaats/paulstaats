a5b3b53113c53d7692d9038b77522ba21b2e3944
Marion Staats <marion.staats@intix.eu>
2023-11-02 20:19:24 +0100

    Add clients and Client page

---
+++ src/composables/clients.ts
@@ -0,0 +1,139 @@
+export type Client = {
+  location: string
+  description: string
+}
+
+export const clients: Client[] = [
+  {
+    location: 'ZZG Zorggroep',
+    description: 'Ontwerp en Supervisie Ontwikkeling Park Dekkerswald te Groesbeek'
+  },
+  {
+    location: 'Helicon te Velp',
+    description: 'Onderwijs en examinator opleiding Urban Design'
+  },
+  {
+    location: 'Ontwikkelaar Junco',
+    description: 'Ontwerp buitenruimte Medisch Centrum in Zwolle en complex in Vaassen'
+  },
+  {
+    location: 'Pannenkoekhuis Reuvershoeve',
+    description: 'Bestemmingsplan en inrichtingsplan, Brummen'
+  },
+  {
+    location: 'Gemeente Rheden',
+    description: 'Begeleiding inspraak herinrichting Van Kolplein, Velp'
+  },
+  {
+    location: 'Particulier',
+    description: 'Ontwerp en advisering landgoed in gemeente Zevenaar'
+  },
+  {
+    location: 'Gezondheidscentrum Hazenkamp',
+    description: 'Massastudie Vossenlaan, Nijmegen'
+  },
+  {
+    location: 'Landgoed De Wiersse',
+    description: 'Bestemmingsplan te Bronckhorst'
+  },
+  {
+    location: 'Gemeente Westervoort',
+    description: 'Ontwerpstudies voor Huis Hamerden, De Nieuwhof en Beekenoord'
+  },
+  {
+    location: 'Hurenkamp Architecten',
+    description: 'Ruimtelijke Onderbouwingen voor kantoortransformaties in Arnhem'
+  },
+  {
+    location: 'Gemeente Voorst',
+    description: 'Massastudies Twello, Terwolde; plannen Dorpszicht en De Vecht'
+  },
+  {
+    location: 'Particulier',
+    description: 'Ontwerp en bouwplanbegeleiding voormalig agrarisch vastgoed in Brummen'
+  },
+  {
+    location: 'Gemeente Arnhem',
+    description: 'Ontwerp en procesbegeleiding basisschool in Arnhem-Zuid'
+  },
+  {
+    location: 'Ontwikkelaar',
+    description: 'Bestemmingsplan Bergstraat 280 te Arnhem'
+  },
+  {
+    location: 'Gemeente Oude IJsselstreek',
+    description: 'Ruimtelijke studie Terborg, Walstraat 22-24'
+  },
+  {
+    location: 'Landschapsarchitect Bijkerk',
+    description: 'Bestemmingsplan Erve Teussink in De Lutte, Losser'
+  },
+  {
+    location: 'Ontwikkelaar',
+    description: 'Ruimtelijke Onderbouwing HKA-terrein en Willemsplein 11, Arnhem'
+  },
+  {
+    location: 'Park Lingezegen',
+    description: 'Burgerparticipatieproject in omgeving van Elst en Lent'
+  },
+  {
+    location: 'Boerstal Bouw',
+    description: 'Ruimtelijke Onderbouwing locatie Emmauskerk te ‘s-Heerenberg, Montferland'
+  },
+  {
+    location: 'Gemeente Putten',
+    description: 'Begeleiding inspraak groenrenovatie'
+  },
+  {
+    location: 'Particulier',
+    description: 'Bestemmingsplan Van Speykplein 5a, Borne'
+  },
+  {
+    location: 'Woningcorporatie Welbions',
+    description: 'Studie woningbouw in Marktstraat, Hengelo'
+  },
+  {
+    location: 'Strategie Architecten',
+    description: 'Ruimtelijke Onderbouwingen voor kantoortransformaties, Arnhem en Zaandam'
+  },
+  {
+    location: 'Stichting Huis Deelerwoud',
+    description: 'Toekomstvisie Landgoed Deelerwoud te Hoenderloo, Apeldoorn'
+  },
+  {
+    location: 'Landgoed Middachten',
+    description: 'Bestemmingsplan met inrichtingsplan, De Steeg'
+  },
+  {
+    location: 'Werkorganisatie CGM',
+    description: 'Gedetacheerd als stedebouwkundige'
+  },
+  {
+    location: 'Loenderslootgroep',
+    description: 'Inpassing Snelfietsroute F28 Amersfoort-Utrecht'
+  },
+  {
+    location: 'Gemeente Lingewaard',
+    description: 'Massastudie voor ontwikkellocatie'
+  },
+  {
+    location: 'Frans op den Bult',
+    description: 'Bestemmingsplan Landgoed op den Bult in Deurningen, Dinkelland'
+  },
+  {
+    location: 'Ontwikkelaar',
+    description: 'Ruimtelijke Onderbouwing Centrum Zaandam'
+  },
+  {
+    location: 'Particulieren',
+    description: 'Haalbaarheidsstudies insectenkwekerij, Eibergen en Haaksbergen'
+  },
+  {
+    location: 'Gemeente Steenwijkerland',
+    description: 'Pilot Omgevingsplan Buitengebied'
+  },
+  {
+    location: 'Gelders Genootschap',
+    description: 'Lid van verschillende Commissies Ruimtelijke Kwaliteit'
+  }
+]
