4d7e28acb2e8a6906142bbe3affa53b7d29e0daf
Marion Staats <marion.staats@intix.eu>
2023-11-03 14:19:55 +0100

    Fix height cover image

--- src/components/CoverImage.vue
+++ src/components/CoverImage.vue
@@ -1,14 +1,17 @@
 <script setup lang="ts">
 import type { PropType } from 'vue'
+import { computed } from 'vue'
 
 defineProps({
   images: { type: Array as PropType<any[]>, required: true }
 })
+
+const height = computed(() => (window.innerHeight * 2) / 3)
 </script>
 
 <template>
-  <div style="height: 600px" class="mb-8">
-    <v-carousel interval="6999" cycle hide-delimiters :show-arrows="false">
+  <div style="height: calc(100vh * 2 / 3)" class="mb-8">
+    <v-carousel interval="6999" cycle hide-delimiters :show-arrows="false" :height="height">
       <v-carousel-item v-for="(item, i) in images" :key="i" :src="item" cover></v-carousel-item>
     </v-carousel>
   </div>
