bd367e9ecbb485ee0df6d05d35238be98dd36b71
Marion Staats <marion.staats@intix.eu>
2023-11-09 16:02:08 +0100

    Update footer mobile

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -4,13 +4,23 @@ import signatureBlack from '@/assets/images/handtekening-ps-black.png'
 import signatureWhite from '@/assets/images/handtekening-ps-wit.png'
 import { useThemeStore } from '@/stores/themeStore'
 import { storeToRefs } from 'pinia'
+import triangleBlack from '@/assets/images/triangle-right-black.svg'
+import triangleWhite from '@/assets/images/triangle-right-white.svg'
 
 const themeStore = useThemeStore()
 const { dark } = storeToRefs(themeStore)
 </script>
 
 <template>
-  <v-footer v-if="isMobile" :height="50" app>
-    <v-img :src="dark ? signatureWhite : signatureBlack"
+  <v-footer v-if="isMobile" :height="50" app class="px-0">
+    <v-img
+      class="signature"
+      :src="dark ? signatureWhite : signatureBlack"
+      height="50px"
+      width="100px" /><v-img
+      class="logo"
+      :src="dark ? triangleBlack : triangleWhite"
+      height="45px"
+      width="45px"
   /></v-footer>
 </template>
