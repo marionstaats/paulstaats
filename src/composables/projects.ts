4be3920bb0b984d91c49c34935baa0705694da48
Marion Staats <marion.staats@intix.eu>
2023-11-11 16:49:15 +0100

    Filter projects per subtype

--- src/composables/projects.ts
+++ src/composables/projects.ts
@@ -23,7 +23,7 @@ export const projects: Project[] = [
     text:
       'Vanaf 2000 is de gemeente Rheden actief met de uitgifte van kavels op het bedrijventerrein De Beemd in Velp. Verantwoordelijk voor het stedebouwkundig ontwerp, het bestemmings- en beeldkwaliteitsplan. Tot 2007 ontwerper voor de buitenruimte en supervisor voor de architectuur. Inmiddels is het terrein bijna volledig uitgegeven, op twee kavels na.' +
       '\n\nDe zeer diverse bedrijven vormen compacte bouwblokken en kennen verzorgde gevels aan alle buitenzijden. De open profielen laten het zicht mogelijk op de IJsseldijk en het buitengebied. Tegelijk zijn veel bedrijven goed zichtbaar vanaf de A348. De bomenlaan en de watering geven een sterke samenhang van noord naar zuid.',
-    link: 'http://paulstaats.com/wp-content/uploads/2018/06/Beeldkwaliteitsplan-De-Beemd.pdf',
+    link: '/Beeldkwaliteitsplan-De-Beemd.pdf',
     linkText: 'Link naar Beeldkwaliteitsplan De Beemd.',
     coverImage: 'GrootDeBeemt.jpg',
     image: '1MidDeBeemt.jpg',
--- src/pages/Work.vue
+++ src/pages/Work.vue
@@ -1,24 +1,35 @@
 <script setup lang="ts">
 import { ref } from 'vue'
 import ProjectCard from '@/components/ProjectCard.vue'
-import { workNav } from '@/composables/navigation'
+import { workNav, WorkTypes } from '@/composables/navigation'
 import router from '@/router'
 import { Routes } from '@/router/routes'
+import type { Project } from '@/composables/projects'
 import { projects } from '@/composables/projects'
 
-const tab = ref(1)
+const tab = ref(null)
+
+const activeProjects = (workType: WorkTypes) =>
+  workType === WorkTypes.ALL
+    ? projects
+    : projects.filter((project: Project) => project.type.includes(workType))
 </script>
 
 <template>
   <v-container>
     <v-tabs show-arrows slider-color="primary" v-model="tab" align-tabs="center">
-      <v-tab v-for="({ title }, i) in workNav" :key="i + 1" :value="i + 1">{{ title }}</v-tab>
+      <v-tab v-for="{ title } in workNav" :key="title" :value="title">{{ title }}</v-tab>
     </v-tabs>
     <v-window v-model="tab">
-      <v-window-item v-for="n in 5" :key="n" :value="n">
+      <v-window-item v-for="{ title } in workNav" :key="title" :value="title">
         <v-container fluid>
           <v-row>
-            <v-col v-for="(project, i) in projects" :key="i" cols="12" md="3">
+            <v-col
+              v-for="(project, i) in activeProjects(title as WorkTypes)"
+              :key="i"
+              cols="12"
+              md="3"
+            >
               <ProjectCard
                 :project="project"
                 @click="
